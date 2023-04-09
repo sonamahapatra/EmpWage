7f637126223d045dbcb9d1ceb331e17bae81bb44
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 12:24:02 +0530

    [sonamahapatra] solved using switch case

--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,33 +11,33 @@ namespace Empwage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Employee Wage Program");
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            
+             const int IS_PART_TIME = 1;
+             const int IS_FULL_TIME = 2;
+             const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random= new Random();
-            int empCheck = random.Next(0,2);
-            //UC3
-            if(empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part Time Present");
-                empHrs = 4;
-            }
-            else if(empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Full Time Present");
-                empHrs= 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
+            int empCheck = random.Next(0,3);
+            //UC4 Switch Case
+           switch (empCheck)
+           {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+           }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EmpWage : " + empWage);
+            Console.WriteLine("Employee wage is:"+empWage);
            Console.ReadLine();
-        }
-    }
+       }
+   }
 }
+    
+
