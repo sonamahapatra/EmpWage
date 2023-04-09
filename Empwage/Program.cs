c43c4a955825580dc41eb30bff80c3d70a9b811f
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 12:38:58 +0530

    [sonamahapatra] Calculating wage for a month

--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -15,26 +15,33 @@ namespace Empwage
              const int IS_PART_TIME = 1;
              const int IS_FULL_TIME = 2;
              const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
             Random random= new Random();
             int empCheck = random.Next(0,3);
-            //UC4 Switch Case
-           switch (empCheck)
-           {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                //UC4 Switch Case
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
-           }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage is:"+empWage);
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is:" + empWage);
+            }
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
            Console.ReadLine();
        }
    }
