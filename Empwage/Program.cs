260e268577e39e65af49cdafdbfa6cb0ab423566
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 11:55:05 +0530

    [sonamahapatra] Adding Part time Employee Wage

--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -11,20 +11,28 @@ namespace Empwage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Employee Wage Program");
-          
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random= new Random();
             int empCheck = random.Next(0,2);
-            if(empCheck == IS_FULL_TIME)
+            //UC3
+            if(empCheck == IS_PART_TIME)
             {
-                empHrs = 8;
+                Console.WriteLine("Employee is Part Time Present");
+                empHrs = 4;
+            }
+            else if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Full Time Present");
+                empHrs= 8;
             }
             else
             {
-                empHrs= 0;
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("EmpWage : " + empWage);
